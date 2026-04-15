c2fd4b123ebb00eddb71d59104caa5ba4893c8e4
Seth Sullivan <sethsullivan99@gmail.com>
2026-04-15 16:40:12 -0400

    polishing off cancel credit project

--- finance/sdt_cancel_credit.py
+++ finance/sdt_cancel_credit.py
@@ -1,3 +1,4 @@
+# %%
 import numpy as np
 import matplotlib.pyplot as plt
 import polars as pl
@@ -6,8 +7,22 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_curve, roc_auc_score
 import xgboost as xgb
 import data_visualization as dv 
+import src.plot_functions as pf
+from sklearn.linear_model import LogisticRegression
+from copy import deepcopy
 
 dv.set_plot_style("cashaback_dark.mplstyle")
+
+def compound_interest(P, r, n):
+    return P*(1 + r/n)**n
+
+def monthly_payment(P, r, n):
+    num = P * r*(1+r)**n 
+    denom = (1+r)**n - 1
+    return num/denom
+
+def interest_paid(P,r,n):
+    return (monthly_payment(P,r,n)*n) - P
 
 # %% Import data
 dtypes = {
@@ -48,15 +63,21 @@ df = df.with_columns(
     pl.when(pl.col("gender") == pl.lit("Female")).then(1).otherwise(0).alias("gender"),
 )
 # %%
+# Make dependents integer column, need to strip the +, not super necessary information
+df = df.with_columns(
+    dependents = pl.col("dependents").str.replace(r"\+","").cast(pl.Int64)
+)
+#
 # Fill missing values with median and add defaulted as a column
 df = df.with_columns(
     pl.col("loanamount").fill_null(pl.col("loanamount").median()),
+    pl.col("dependents").fill_null(pl.col("dependents").median()),
     pl.col("loan_amount_term").fill_null(pl.col("loan_amount_term").median()),
     pl.col("credit_history").fill_null(pl.col("credit_history").median()),
     pl.when(pl.col("loan_status") == 1).then(0).otherwise(1).alias("defaulted"),
 )
-print(df)
 
+print(df)
 # %% Decide how much capital
 # The bank wants to be 95% confident it can cover a loss of X dollars
 # Two things: prob_default and loan_loss to get expected loss
@@ -71,15 +92,16 @@ print(df)
 # 1. Select a prob_default cutoff and simulate expected loss and profits 
 
 # %% Train/test split
-df = df.to_pandas()
+if not isinstance(df, pd.DataFrame):
+    df = df.to_pandas()
 X = df.drop(
-    columns=["defaulted", "loan_status", "loan_id", "dependents", "property_area"]
+    columns=["defaulted", "loan_status", "loan_id", "property_area"]
 )
 y = df["defaulted"]
-FEATURES = list(X.columns)
+FEATURES = deepcopy(list(X.columns))
 
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, random_state=123, test_size=0.25
+    X, y, random_state=123, test_size=0.3
 )
 # Only doing this so pyright stops yelling
 X_train = pd.DataFrame(X_train)
@@ -90,22 +112,25 @@ majority_instances = np.sum(y_train == 0)
 minority_instances = np.sum(y_train == 1)
 scale_pos_weight = majority_instances / minority_instances
 clf = xgb.XGBClassifier(
-    n_estimators=500,
-    learning_rate=0.05,
-    max_depth=5,
+    n_estimators=100,
+    learning_rate=0.1,
+    max_depth=2,
+    # gamma=2,
     # n_features=2,
     scale_pos_weight=scale_pos_weight,
     objective="binary:logistic",
     eval_metric="auc",
+    reg_alpha=0.5,
+    reg_lambda=2,
 )
 
-cols_to_use = FEATURES
+cols_to_use = ["dependents", "applicantincome", "coapplicantincome", "loanamount", "credit_history"]
 _ = clf.fit(
     X_train.loc[:, cols_to_use],
     y_train,
     eval_set=[(X_train[cols_to_use], y_train), (X_test[cols_to_use], y_test)],
     verbose=100,
-)  #
+) 
 
 # Print auc for all data, this is cheating bc i'm predicting on what i trained but whatever
 preds = clf.predict_proba(X[cols_to_use])[
@@ -113,6 +138,57 @@ preds = clf.predict_proba(X[cols_to_use])[
 ]  # Second index because that's 1, meaning they did default
 auc = roc_auc_score(y, preds)
 print(auc)
+
+xg_train_preds = clf.predict_proba(X_train[cols_to_use])[:, 1]
+xg_test_preds = clf.predict_proba(X_test[cols_to_use])[:, 1]
+fpr_train, tpr_train, _ = roc_curve(y_train, xg_train_preds)
+fpr_test, tpr_test, _ = roc_curve(y_test, xg_test_preds)
+train_auc = roc_auc_score(y_train, xg_train_preds)
+test_auc = roc_auc_score(y_test, xg_test_preds)
+
+# Plot roc auc
+fig, ax = plt.subplots()
+pf.plot_roc(ax, fpr_train, tpr_train, fpr_test, tpr_test, train_auc, test_auc, plot_unity=False)
+ax.set_title("XGBoost Classifier")
+
+
+
+plt.show()
+
+# Feature Importance, 
+fig, ax = plt.subplots(figsize=(6.5, 4))
+xlocs = np.arange(0, len(cols_to_use))
+ax.bar(xlocs, clf.feature_importances_)
+ax.set_xticks(xlocs, labels=cols_to_use, rotation=45)
+ax.set_ylabel("Importance")
+plt.tight_layout()
+plt.show()
+
+# %% Try logistic
+if True:
+
+    logistic = LogisticRegression(
+        max_iter=10000, l1_ratio=0, solver="lbfgs", class_weight="balanced"
+    ).fit(X_train[cols_to_use], y_train)
+    score = logistic.score(X_train[cols_to_use], y_train)
+    print("Logistic Regression Accuracy on Training data:", score)
+
+    score = logistic.score(X_test[cols_to_use], y_test)
+    print("Logistic Regression Accuracy on Testing data:", score)
+
+    log_train_preds = logistic.predict_proba(X_train[cols_to_use])[:,1]
+    log_test_preds = logistic.predict_proba(X_test[cols_to_use])[:,1]
+    fpr_train, tpr_train, _ = roc_curve(y_train, log_train_preds)
+    fpr_test, tpr_test, _ = roc_curve(y_test, log_test_preds)
+    train_auc = roc_auc_score(y_train, log_train_preds)
+    test_auc = roc_auc_score(y_test, log_test_preds)
+
+    # Plot roc auc
+    fig, ax = plt.subplots()
+    pf.plot_roc(ax, fpr_train, tpr_train, fpr_test, tpr_test, train_auc, test_auc)
+    ax.set_title("Logistic Regression")
+    
+    plt.show()
 
 # %% Get expected loss
 
@@ -153,13 +229,13 @@ print(f"Bank can be 95% confident that losses won't exceed {conf_interval[1]}")
 
 #%% Now use probabilities to find optimal policy
 # Max reward and minimize expected loss
-
+log_preds = logistic.predict_proba(X[cols_to_use])[:,1]
+df["prob_default"] = preds
 # profits is the interest rate on the loan 
-INT_RATE = 0.3
-recovery_rate = 0.4 # Hard coded, this should be modeled
-df['prob_default'] = preds
-# 12 months of accumulating interest, this is assuming that they don't pay off the loan, and just pay the interest along the way
-df["expected_profit"] = df['loanamount']*(1 - df["prob_default"])*INT_RATE
+INT_RATE = 0.05
+recovery_rate = 0.5 # Hard coded, thing that they don't pay off the loan, and just pay the interest along the way
+loan_months = df['loan_amount_term']/30 # get term in months
+df["expected_profit"] = interest_paid(df['loanamount'], INT_RATE, loan_months)*(1 - df["prob_default"])
 # EAD is loan amount (not always true, people might pay back part of the loan, but doing this to simplify)
 # PD is prob_default calculated by the model
 # LGD is (1 - Recovery Rate)
@@ -181,25 +257,29 @@ fig.axes['b'].scatter(df['prob_default'], df['expected_loss'])
 fig.axes['c'].scatter(df['prob_default'], df['expected_gain'])
 
 # %% Simulate with different prob cutoffs
-prob_default_cutoffs = np.arange(0.02,1,0.01)
+prob_default_cutoffs = np.arange(0.0,1,0.01)
 expected_profits = []
+expected_profits_dist = []
 expected_losses = []
 expected_gains = []
 for cutoff in prob_default_cutoffs:
     new_df = df[df['prob_default']<cutoff]
-    print(len(new_df))
+    # print(len(new_df))
     expected_profits.append(new_df['expected_profit'].sum())
+    expected_profits_dist.append(new_df['expected_profit'])
     expected_losses.append(new_df['expected_loss'].sum())
     expected_gains.append(new_df['expected_gain'].sum())
-    
+opt_idx = np.argmax(expected_profits)
+max_profits = np.max(expected_profits)    
+
 # %% 
-fig = dv.AutoFigure('a;b;c', figsize=(7,7))
+fig = dv.AutoFigure('a;b;c', figsize=(5,5))
 
 fig.axes['a'].plot(expected_profits)
 fig.axes['b'].plot(expected_losses)
 fig.axes['c'].plot(expected_gains)
-fig.axes['c'].axvline(np.argmax(expected_gains), ymin=0, ymax = 0.63)
-fig.axes['c'].text(np.argmax(expected_gains),np.max(expected_gains), 
+fig.axes['c'].plot((np.argmax(expected_gains), np.argmax(expected_gains)), (0,np.max(expected_gains)), color='white')
+fig.axes['c'].text(np.argmax(expected_gains),np.max(expected_gains)+750, 
                    rf"Cutoff Policy"+ "\n" + rf"($\tau = {np.argmax(expected_gains)}$%)", 
                    ha='center', va='bottom', fontweight='bold')
 
@@ -212,4 +292,49 @@ for ax in fig.axes.values():
     ax.set_xlabel("Probability of Default Cutoff (%)")
 fig.remove_figure_borders()
        
+# %%
+optimal_cutoff = prob_default_cutoffs[opt_idx]
+opt_df = df[df['prob_default']<opt_idx]
+opt_loan_amounts = opt_df['loanamount']
+opt_prob_default = opt_df['prob_default']
+opt_loan_months = opt_df['loan_amount_term']
+
+n = 10000
+mean_losses = []
+mean_profits = []
+mean_pl = []
+for i in range(n):
+    # Roll a 1 or 0 based on our distribution of people
+    defaults = np.random.binomial(n=1, p=opt_prob_default)
+    non_defaults = 1 - defaults
+                                  
+    # Calculate loss
+    loss = defaults * opt_loan_amounts * (1 - recovery_rate)
+    mean_losses.append(np.sum(loss))
+    profit = interest_paid(opt_loan_amounts*non_defaults, INT_RATE, loan_months*non_defaults)
+    mean_profits.append(np.sum(profit))
+    mean_pl.append(np.sum(profit)-np.sum(loss))
+
+conf_interval = [np.percentile(mean_losses, 2.5), np.percentile(mean_losses, 97.5)]
+fig, (ax1,ax2,ax3) = plt.subplots(3, figsize=(4,3))
+ax1.hist(mean_losses, bins=100)
+ax2.hist(mean_profits, bins=100)
+ax3.hist(mean_pl, bins=100)
+ax1.axvline(conf_interval[0], color="white")
+ax1.axvline(conf_interval[1], color="white")
+fig.suptitle("Monte Carlo Simulation")
+plt.show()
+# %% Just plot losses
+
+fig,ax = plt.subplots()
+
+ax.hist(mean_losses, bins=50)
+ax.axvline(conf_interval[0], color="white")
+ax.axvline(conf_interval[1], color="white")
+
+ax.set_title("Monte Carlo Simulation")
+
+ax.set_xlabel("Loss")
+ax.set_ylabel("Counts")
+
 # %%
--- src/plot_functions.py
+++ src/plot_functions.py
@@ -1,7 +1,8 @@
 import numpy as np 
 
-def plot_roc(ax, fpr_train, tpr_train, fpr_test, tpr_test, train_auc, test_auc):
-    ax.plot(np.arange(0, 1.1, 0.1), np.arange(0, 1.1, 0.1), color="grey")
+def plot_roc(ax, fpr_train, tpr_train, fpr_test, tpr_test, train_auc, test_auc, plot_unity=True):
+    if plot_unity:
+        ax.plot(np.arange(0, 1.1, 0.1), np.arange(0, 1.1, 0.1), color="grey")
     ax.plot(fpr_train, tpr_train, label="train performance")
     ax.plot(fpr_test, tpr_test, label="test performance")
     ax.text(0.85, 0.1, f"Train AUC: {train_auc:.4f}")
